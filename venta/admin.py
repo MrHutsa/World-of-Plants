85bbc0511b95c51993757969fd8a23739e0e4215
MrHutsa <129012843+MrHutsa@users.noreply.github.com>
2023-06-16 00:59:58 -0400

    -

--- venta/admin.py
+++ venta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Usuario
+from .models import Usuario, Producto
 
 # Register your models here.
 #admin.site.register(Genero)
 admin.site.register(Usuario)
+admin.site.register(Producto)
